c28c8c84b34aee2e5a11c97598116784544223c1
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-05 13:24:13 -0600

    add layout

--- src/screens/Settings.jsx
+++ src/screens/Settings.jsx
@@ -1,20 +1,13 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import COLORS from "@constants/Colors";
+import { Layout } from "../layout/Layout";
 
 export const Settings = () => {
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        backgroundColor: COLORS.lightTheme.primaryColor,
-        paddingTop: 50,
-      }}
-    >
-      <View style={styles.container}>
-        <Text>Settings</Text>
-      </View>
-    </ScrollView>
+    <Layout view>
+      <Text>Settings</Text>
+    </Layout>
   );
 };
 
