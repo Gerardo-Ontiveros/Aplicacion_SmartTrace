29769123b186d243441e5a129df99529566ae6c2
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-05 12:32:35 -0600

    Crear layaout default

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
+import COLORS from "@constants/Colors";
+
+export const Layout = ({ children, view }) => {
+  return (
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.lightTheme.primaryColor,
+        paddingTop: 50,
+      }}
+    >
+      {view ? <View style={styles.container}>{children}</View> : children}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 25,
+  },
+});
