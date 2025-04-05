317435726b266ca8da0e4a2d04f4a5ec2921a9a7
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-05 09:50:11 -0600

    Nuevo color

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,9 +1,10 @@
 const COLORS = {
   lightTheme: {
     primaryColor: "#4EA3DB",
-    secondaryColor: "#3494D6",
+    secondaryColor: "#2095e3",
     thirdColor: "rgba(255, 255, 255, 0.2)", //THIS COLOR HAS 20% OPACITY
     whiteColor: "#FFFFFF",
+    foucsIcon: "#000d15",
   },
   darkTheme: {
     primaryColor: "#28292E",
