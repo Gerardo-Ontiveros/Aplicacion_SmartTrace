19e3c93600a42371e7cad132203bcffa599cd841
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-05 09:27:25 -0600

    Colores utilizados

---
+++ src/constants/Colors.js
@@ -0,0 +1,16 @@
+const COLORS = {
+  lightTheme: {
+    primaryColor: "#4EA3DB",
+    secondaryColor: "#3494D6",
+    thirdColor: "rgba(255, 255, 255, 0.2)", //THIS COLOR HAS 20% OPACITY
+    whiteColor: "#FFFFFF",
+  },
+  darkTheme: {
+    primaryColor: "#28292E",
+    secondaryColor: "#3494D6",
+    thirdColor: "#FFFFFF", //THIS COLOR HAS 20% OPACITY
+    whiteColor: "#FFFFFF",
+  },
+};
+
+export default COLORS;
