010204241ab233472388054688db935e4a8fc1b2
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-05 12:23:46 -0600

    Rediseño de tab navigator

--- src/routes/screens.routes.js
+++ src/routes/screens.routes.js
@@ -1,100 +1,111 @@
+import React, { useRef, useEffect } from "react";
+import { View, StyleSheet, Animated } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import COLORS from "@constants/Colors";
 import { Home } from "@screens/Home";
-import { Temperature } from "@screens/Temperature";
 import { Pressure } from "@screens/Pressure";
 import { Settings } from "@screens/Settings";
-import Icon from "react-native-vector-icons/Feather";
-import Icon2 from "react-native-vector-icons/FontAwesome6";
-import Icon3 from "react-native-vector-icons/MaterialCommunityIcons";
-import { LinearGradient } from "expo-linear-gradient";
-import COLORS from "@constants/Colors";
+import { Temperature } from "@screens/Temperature";
 
 const Tab = createBottomTabNavigator();
 
-export const TabNavigator = () => {
+const AnimatedDot = ({ focused }) => {
+  const opacity = useRef(new Animated.Value(0)).current;
+  const translateY = useRef(new Animated.Value(4)).current;
+
+  useEffect(() => {
+    if (focused) {
+      Animated.parallel([
+        Animated.timing(opacity, {
+          toValue: 1,
+          duration: 200,
+          useNativeDriver: true,
+        }),
+        Animated.timing(translateY, {
+          toValue: 0,
+          duration: 200,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    } else {
+      Animated.parallel([
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 150,
+          useNativeDriver: true,
+        }),
+        Animated.timing(translateY, {
+          toValue: 4,
+          duration: 150,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    }
+  }, [focused]);
+
+  return (
+    <Animated.View
+      style={[
+        styles.dot,
+        {
+          opacity,
+          transform: [{ translateY }],
+        },
+      ]}
+    />
+  );
+};
+
+export default function TabNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
           backgroundColor: COLORS.lightTheme.primaryColor,
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 70,
+          borderTopWidth: 0,
         },
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused }) => (
-            <Icon
-              name="home"
-              size={24}
-              color={
-                focused
-                  ? COLORS.lightTheme.focusIcon
-                  : COLORS.lightTheme.whiteColor
-              }
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Temperature"
-        component={Temperature}
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused }) => (
-            <Icon2
-              name="temperature-quarter"
-              size={24}
-              color={
-                focused
-                  ? COLORS.lightTheme.focusIcon
-                  : COLORS.lightTheme.whiteColor
-              }
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Pressure"
-        component={Pressure}
-        options={{
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused }) => (
-            <Icon3
-              name="car-brake-low-pressure"
-              size={24}
-              color={
-                focused
-                  ? COLORS.lightTheme.focusIcon
-                  : COLORS.lightTheme.whiteColor
-              }
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          tabBarShowLabel: false,
+        tabBarIcon: ({ focused }) => {
+          let iconName;
 
-          tabBarIcon: ({ focused }) => (
-            <Icon
-              name="settings"
-              size={24}
-              color={
-                focused
-                  ? COLORS.lightTheme.foucsIcon
-                  : COLORS.lightTheme.whiteColor
-              }
-            />
-          ),
-        }}
-      />
+          if (route.name === "Home") iconName = "home-outline";
+          else if (route.name === "Temperature")
+            iconName = "thermometer-outline";
+          else if (route.name === "Pressure") iconName = "compass-outline";
+          else if (route.name === "Settings") iconName = "settings-outline";
+
+          return (
+            <View style={styles.iconContainer}>
+              <Ionicons name={iconName} size={28} color="#fff" />
+              <AnimatedDot focused={focused} />
+            </View>
+          );
+        },
+        tabBarShowLabel: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Temperature" component={Temperature} />
+      <Tab.Screen name="Pressure" component={Pressure} />
+      <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dot: {
+    width: 6,
+    height: 6,
+    borderRadius: 999,
+    backgroundColor: "#fff",
+    marginTop: 4,
+  },
+});
