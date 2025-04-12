86189dc61cdc3c75fc1280d5ea471844370ee910
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-11 19:34:54 -0600

    Create Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
-import COLORS from "@constants/Colors";
+import React from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import COLORS from '@constants/Colors';
 
 export const Layout = ({ children, view }) => {
   return (
     <ScrollView
       style={{
         flex: 1,
+        height: '100%',
         backgroundColor: COLORS.lightTheme.primaryColor,
         paddingTop: 50,
       }}
@@ -18,8 +19,8 @@ export const Layout = ({ children, view }) => {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingTop: 25,
   },
 });
