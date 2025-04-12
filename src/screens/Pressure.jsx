d3e88e1c882ab3106a860460db6cf86a2828e2e9
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-04-11 19:35:02 -0600

    agregar datos

--- src/screens/Pressure.jsx
+++ src/screens/Pressure.jsx
@@ -1,11 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Layout } from "../layout/Layout";
+import React from 'react';
+import { CardPressureList } from '@components/CardPressureList';
+import { Layout } from '../layout/Layout';
+
+const lastUpdate = [
+  {
+    id: 1,
+    pressure: 22,
+    altitude: 11,
+  },
+  {
+    id: 2,
+    pressure: 22,
+    altitude: 11,
+  },
+  {
+    id: 3,
+    pressure: 22,
+    altitude: 11,
+  },
+  {
+    id: 4,
+    pressure: 22,
+    altitude: 11,
+  },
+  {
+    id: 5,
+    pressure: 20,
+    altitude: 4,
+  },
+];
 
 export const Pressure = () => {
   return (
     <Layout view>
-      <Text>Pressure</Text>
+      {lastUpdate.map((item) => (
+        <CardPressureList key={item.id} pressure={item.pressure} altitude={item.altitude} />
+      ))}
     </Layout>
   );
 };
